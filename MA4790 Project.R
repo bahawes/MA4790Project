e4b35eb00886656934616c79da9965df0db84c4b
bhawes24 <64669597+bhawes24@users.noreply.github.com>
2022-09-29 13:42:33 -0400

    Removed some bad variables

--- MA4790 Project.R
+++ MA4790 Project.R
@@ -1,11 +1,32 @@
-football <- read.csv("footballData.csv")
+footballPre <- read.csv("footballData.csv")
 
 #### Data Cleaning ####
 
+## pref foot -> 1,0
+
+## Dropped: sofifa_id, player_url, short_name, long_name,
+  ## team_position, national_position, national_jersey_number
+  ## body_type
+
+## Drop: player_tags, player_traits, real_face?
+  ## All Goalies? Just focus on players?
+  ## Or remove all predictors exclusive to one position?
+Drop <- c(1,2,3,4,23,27,32,33)
+football <- footballPre[-Drop]
+
+
+## How to clean position?
+  ## Var for each position, 0 or 1?
+
+## Split work rate into 2
+
+## what does realface mean?
+  ## Drop realface?
+
 #### Explore Data ####
-# Goal
-# to predict a players value from our predictors
-# Data Structure
+## Goal
+  ## to predict a players value from our predictors
+## Data Structure
 dim(football)
 str(football)
 
@@ -21,10 +42,10 @@ intCount <- dim(football)[2] - charCount
 charCount
 intCount
 
-# Sample Size: 18944
-# Response Variable: value_euro
-# Number of predictors: 105? (remove predictors that don't make sense like player url and name)
-# Categorical: 44?
-# Continuous: 61?
+## Sample Size: 
+## Response Variable: value_euro
+## Number of predictors: 
+## Categorical: 
+## Continuous: 
 
 #### Data Preprocessing ####
