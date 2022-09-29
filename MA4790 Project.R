f50d8fed7dd16972ecae825e8304bd42665622a9
bhawes24 <64669597+bhawes24@users.noreply.github.com>
2022-09-29 09:42:14 -0400

    Data Exploration

---
+++ MA4790 Project.R
@@ -0,0 +1,27 @@
+football <- read.csv("footballData.csv")
+#### Explore Data ####
+# Goal
+# to predict a players value from our predictors
+# Data Structure
+dim(football)
+str(football)
+
+charCount <- 0
+for (i in football) {
+  if (typeof(i) == "character") {
+    charCount <- charCount + 1
+  }
+}
+
+intCount <- dim(football)[2] - charCount
+
+charCount
+intCount
+
+# Sample Size: 18944
+# Response Variable: value_euro
+# Number of predictors: 105? (remove predictors that don't make sense like player url and name)
+# Categorical: 44?
+# Continuous: 61?
+
+#### Data Preprocessing ####
