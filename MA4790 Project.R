a691199b9b2c4be9b2725d70fcf1903af3345534
bhawes24 <64669597+bhawes24@users.noreply.github.com>
2022-09-30 16:47:18 -0400

    adding variables

--- MA4790 Project.R
+++ MA4790 Project.R
@@ -9,23 +9,90 @@ footballPre <- read.csv("footballData.csv")
   ## All Goalies rows
   ## all predictors exclusive to goalies
 
-## team_position -> sub or not (0 or 1)
-
-## pref foot -> 1,0
-
-## Column for if on national team
-
 Drop <- c(1,2,3,4,6,9,10,23,26,28,30,31,33,40:45,46,73,76:106)
 football <- footballPre[-Drop]
+football <- subset(football,
+                   !grepl("GK",football$player_positions)
+                   )
+
+## team_position -> sub or not (0 or 1)
+for(i in 1:length(football)){
+  
+  if(football$team_position[i] == "SUB"){
+    newValue <- 1
+  }
+  
+  else{
+    newValue <- 0
+  }
+  
+  football$team_position[i] <- newValue
+}
 
 
-## How to clean position?
-  ## Var for each position, 0 or 1?
+## Player_positions -> dummy variables
 
+  # find list of all positions
+  positions <- c()
+  for (i in 1:nrow(football)) {
+    currentPositions <- unlist(strsplit(football$player_positions[i], ","))
+    positions <- append(positions, currentPositions)
+  }
+  positions <- gsub(" ", "", positions)
+  positions <- unique(positions)
+  
+  # set new predictors
+  positions_mat <- matrix(ncol = length(positions))
+  for (i in 1:nrow(football)) {
+    currentRow <- c()
+    currentPositions <- unlist(strsplit(football$player_positions[i], ","))
+    
+    for (j in 1:length(positions)) {
+      
+      if (positions[j] %in% currentPositions) {
+        currentRow <- append(currentRow, 1)
+      }
+      
+      else {
+        currentRow <- append(currentRow, 0)
+      }
+      
+      
+    }
+    positions_mat <- rbind(positions_mat, currentRow)
+  }
+  
+  # clean data frame
+  positions_df <- as.data.frame(positions_mat, row.names = FALSE)
+  positions_df <- positions_df[-1,]
+  colnames(positions_df) <- positions
+  
+  # add new predictors and drop original predictor
+  football <- cbind(football, positions_df)
+  football <- football[-10]
+  
+  
+## preferred_foot -> 1,0
+  # left: 0, right: 1
+  football$preferred_foot <- 
+    ifelse(football$preferred_foot == "Left", 0, 1)
+  
+## nation_position -> on_national_team
+  football$nation_position <- 
+    ifelse(football$nation_position == "", 0, 1)
+  colnames(football)[19] <- "on_national_team"
+  
+## national position -> on_national_team
+  
 ## Split work rate into 2
-
-## what does realface mean?
-  ## Drop realface?
+  
+## League Name
+  
+## drop player positions
+  
+## real face
+  
+## loaned from -> on_loan
 
 #### Explore Data ####
 ## Goal
